21a95bc9c25e48d03675d784be6b8e0455daa567
itexpert120 <itexpert120@gmail.com>
2023-06-12 19:07:25 +0500

    changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -165,32 +165,31 @@ export default function Home() {
             </div>
           ) : null}
 
-          <div className="my-4">
-            <h1 className="text-2xl font-bold">Completed Tasks</h1>
-          </div>
           <div className="my-4 flex flex-col gap-2">
             {completedTasks.length !== 0 && (
-              <button
-                type="button"
-                className="text-white text-sm bg-sky-500 hover:bg-sky-600 rounded-lg w-full py-2"
-                onClick={clearCompleted}
-              >
-                Clear Completed
-              </button>
-            )}
-            {completedTasks.length !== 0 ? (
-              completedTasks.map((t: Task, i) => (
-                <>
-                  <div className="border rounded-lg p-4 bg-gray-100">
-                    <div className="flex flex-row justify-between">
-                      <h1 className="font-bold text-lg">{t.title}</h1>
+              <>
+                <div className="my-4">
+                  <h1 className="text-2xl font-bold">Completed Tasks</h1>
+                </div>
+                <button
+                  type="button"
+                  className="text-white text-sm bg-sky-500 hover:bg-sky-600 rounded-lg w-full py-2"
+                  onClick={clearCompleted}
+                >
+                  Clear Completed
+                </button>
+
+                {completedTasks.map((t: Task, i) => (
+                  <>
+                    <div className="border rounded-lg p-4 bg-gray-100">
+                      <div className="flex flex-row justify-between">
+                        <h1 className="font-bold text-lg">{t.title}</h1>
+                      </div>
+                      <p className="text-black my-1">{t.summary}</p>
                     </div>
-                    <p className="text-black my-1">{t.summary}</p>
-                  </div>
-                </>
-              ))
-            ) : (
-              <p className="py-2 text-center text-md">No Tasks Completed</p>
+                  </>
+                ))}
+              </>
             )}
           </div>
         </div>
