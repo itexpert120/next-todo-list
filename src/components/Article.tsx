95584a1ffb6c805fcdd427055a26f010d2c6fbbf
itexpert120 <itexpert120@gmail.com>
2023-06-13 01:36:26 +0500

    minor changes to article

--- src/components/Article.tsx
+++ src/components/Article.tsx
@@ -16,7 +16,12 @@ export function Article(props: TaskProps) {
         <div className="flex flex-row justify-between">
           <h1 className="font-bold text-lg">{props.title}</h1>
           <button type="button" onClick={handleDelete}>
-            <Image src="/trash-full.svg" width="16" height="16" alt="trash" />
+            <Image
+              src="/checkbox-check.svg"
+              width="24"
+              height="24"
+              alt="trash"
+            />
           </button>
         </div>
         <p className="text-black my-1">{props.body}</p>
